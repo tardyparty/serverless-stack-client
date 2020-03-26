7f0e7ceda96e75947453c29a21d2500ea44302fe
tardyparty <tardyparty@users.noreply.github.com>
2020-03-26 18:31:20 -0500

    configuring to my api

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "tardynotes"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://yepa569207.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "US-EAST-2",
+        USER_POOL_ID: "us-east-2_FuYR69NdD",
+        APP_CLIENT_ID: "51qghlui6irsuh27ldsutt97o9",
+        IDENTITY_POOL_ID: "us-east-2:59eaa866-3627-4821-98a4-4b7abaaa93ee"
+    }
+};
