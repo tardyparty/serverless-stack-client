5f6a402cb2ed38b7ff251acc240258df5bd5e310
tardyparty <tardyparty@users.noreply.github.com>
2020-03-26 18:17:58 -0500

    handle 404 errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 
